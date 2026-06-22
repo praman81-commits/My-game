997058eebb51d37fca8214ab7cab86a8638f0fe3
praman81-commits <praman81@kingshigh.school.nz>
2026-06-22 12:26:06 +1200

    animation movement
    v6

--- platformer-game/new-game-project/player.gd
+++ platformer-game/new-game-project/player.gd
@@ -6,7 +6,7 @@ extends CharacterBody2D
 @export var acceleration: float = 15.0
 @export var jump = 1
 
-enum state {IDLE,RUNNING,JUMPUP,JUMPDOWN,DEAD,JUMPATTCK,JUMPTHROW,SLIDE,THROW}
+enum state {IDLE,RUNNING,JUMPUP,JUMPDOWN,DEAD,JUMPATTACK,JUMPTHROW,SLIDE,THROW}
 
 var anim_state = state.IDLE
 
@@ -14,7 +14,7 @@ var anim_state = state.IDLE
 @onready var animation_player = $AnimationPlayer
 
 # Get the gravity from the project setting to be synced with RegidBody  nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_graviy")
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func update_state():
 	if anim_state == state.DEAD:
@@ -35,6 +35,37 @@ func update_animation(direction):
 		animator.flip_h = false
 	elif direction < 0:
 		animator.flip_h = true
+
+
+	if Input.is_action_just_pressed("throw"):
+		if is_on_floor():
+			anim_state = state.THROW
+		else:
+			anim_state = state.JUMPTHROW
+		return
+
+	if Input.is_action_just_pressed("jumpattack") and not is_on_floor():
+		anim_state = state.JUMPATTACK
+		return
+
+	# 3. Your existing normal movement state checks
+	if is_on_floor():
+		if velocity.x == 0:
+			anim_state = state.IDLE
+		else:
+			anim_state = state.RUNNING
+	else:
+		if velocity.y < 0:
+			anim_state = state.JUMPUP
+		else:
+			anim_state = state.JUMPDOWN
+func update_animation(direction):
+	if direction > 0:
+		animator.flip_h = false
+	elif direction < 0:
+		animator.flip_h = true
+		
+	# Move the match block back here!
 	match anim_state:
 		state.IDLE:
 			animation_player.play("idle")
@@ -44,24 +75,57 @@ func update_animation(direction):
 			animation_player.play("jumpup")
 		state.JUMPDOWN:
 			animation_player.play("jumpdown")
-		state.
-	
-	
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta	
+		state.DEAD:
+			animation_player.play("dead")
+		state.JUMPATTACK:
+			animation_player.play("jump attack")
+		state.JUMPTHROW:
+			animation_player.play("jump throw")
+		state.SLIDE:
+			animation_player.play("slide")
+		state.THROW:
+			animation_player.play("throw")
+
+# --- Make sure this function is entirely separate ---
+func update_state():
+	if anim_state == state.DEAD:
+		return
 		
-		# Handle Jump
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
+	# 1. Reset special actions back to normal once their animation finishes
+	if anim_state in [state.SLIDE, state.THROW, state.JUMPATTACK, state.JUMPTHROW]:
+		if not animation_player.is_playing():
+			if is_on_floor():
+				anim_state = state.IDLE
+			else:
+				anim_state = state.JUMPDOWN
+		else:
+			return 
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left","right")
-	if direction:
-		velocity.x = move_toward(velocity.x,direction*speed,acceleration)
+	# 2. Check for inputs
+	if Input.is_action_just_pressed("slide") and is_on_floor():
+		anim_state = state.SLIDE
+		return
+	if Input.is_action_just_pressed("throw"):
+		if is_on_floor():
+			anim_state = state.THROW
+		else:
+			anim_state = state.JUMPTHROW
+		return
+	if Input.is_action_just_pressed("jumpattack") and not is_on_floor():
+		anim_state = state.JUMPATTACK
+		return
+
+	# 3. Normal movement
+	if is_on_floor():
+		if velocity.x == 0:
+			anim_state = state.IDLE
+		else:
+			anim_state = state.RUNNING
 	else:
-		velocity.x = move_toward(velocity.x,0,speed)
-
+		if velocity.y < 0:
+			anim_state = state.JUMPUP
+		else:
+			anim_state = state.JUMPDOWN
+	update_state()
+	update_animation(direction)
 	move_and_slide()
